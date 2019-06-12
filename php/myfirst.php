3daa52843beaf701e0decba816e7859dc80c9f0f
user <sergeyl04082004@gmail.com>
2019-06-12 11:27:22 +0300

    myfirst commit

---
+++ php/myfirst.php
@@ -0,0 +1,20 @@
+<?php
+ 
+$a="helpme";
+$b="please";
+
+print("^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+print($a.$b);
+print("\n");
+
+print($a.",".$b);
+print("\n");
+
+print($a.", ".$b . "!");
+print("\n");
+print("^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+
+
+
+
+?>
